df0d70c69231324b8b10764bfe0018999deef566
Saksham Gera <sakshamgera1456@gmail.com>
2025-06-02 19:39:22 +0530

    Added A zeromq based study and also corrected some existing studies

---
+++ 0mq/funbody_zmq.py
@@ -0,0 +1,45 @@
+# funbody2_zmq.py
+import zmq
+import time
+import concore
+import concore2
+
+print("funbody using 0mq")
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:2346")
+
+concore.delay = 0.07
+concore2.delay = 0.07
+concore2.inpath = concore.inpath
+concore2.outpath = concore.outpath
+concore2.simtime = 0
+concore.default_maxtime(100)
+init_simtime_u = "[0.0, 0.0, 0.0]"
+init_simtime_ym = "[0.0, 0.0, 0.0]"
+
+u = concore.initval(init_simtime_u)
+ym = concore2.initval(init_simtime_ym)
+
+while concore2.simtime < concore.maxtime:
+    msg = socket.recv_json()
+    if msg["action"] == "fun":
+        u = msg["params"]["u"]
+        concore.simtime = u[0]
+        u = u[1:]
+        concore.write(concore.oport['U2'], "u", u)
+        print(u)
+        old2 = concore2.simtime
+        while concore2.unchanged() or concore2.simtime <= old2:
+            ym = concore2.read(concore.iport['Y2'], "ym", init_simtime_ym)
+        ym_full = [concore2.simtime] + ym
+        print(f"Replying with {ym_full}")
+        socket.send_json({"ym": ym_full})
+        print(f"funbody u={u} ym={ym} time={concore2.simtime}")
+    else:
+        print("undefined action: " + str(msg.get("action", "None")))
+        socket.send_json({"error": "undefined action"})
+        break
+
+print("retry=" + str(concore.retrycount))
--- demo/pmFile.py
+++ demo/pmFile.py
@@ -1,42 +1,37 @@
-import concore
+import ast
 import numpy as np
+import concore
 
-#//pm function//
 def pm(u):
-  return u + 0.01
+    return u + 0.01
 
-#//main//
-
-concore.default_maxtime(150) ##maps to-- for i in range(0,150):
+concore.default_maxtime(150)
 concore.delay = 0.02
 
-#//initial values-- transforms to string including the simtime as the 0th entry in the list//
-# u = np.array([[0.0]])
-# ym = np.array([[0.0]]) 
 init_simtime_u = "[0.0, 0.0]"
 init_simtime_ym = "[0.0, 0.0]"
 
 ym = np.array([concore.initval(init_simtime_ym)]).T
-while(concore.simtime<concore.maxtime):
+
+while concore.simtime < concore.maxtime:
     while concore.unchanged():
-        u = concore.read(1,"u",init_simtime_u)
-    u = np.array([u]).T
-    #####
+        u_raw = concore.read(1, "u", init_simtime_u)
+        if isinstance(u_raw, str):  # Fallback when read fails and returns init_simtime_u
+            try:
+                u_raw = ast.literal_eval(u_raw)
+            except:
+                print("Failed to parse fallback string:", u_raw)
+                u_raw = [0.0]  # safe default
+        u = np.array([u_raw], dtype=np.float64).T
+
     try:
-        infile = open(concore.inpath+"1/file.txt")
-        print(infile.read())
-        infile.close()
+        with open(concore.inpath + "1/file.txt") as infile:
+            print(infile.read())
     except:
         print("no file.txt yet")
+
     ym = pm(u)
-    #####
-    print(str(concore.simtime) + ". u="+str(u) + "ym="+str(ym));
-    concore.write(1,"ym",list(ym.T[0]),delta=1)
+    print(f"{concore.simtime}. u={u} ym={ym}")
+    concore.write(1, "ym", list(ym.T[0]), delta=1)
 
-print("retry="+str(concore.retrycount))
-
-
-#// main-- to begin with//
-#for i in range(0,150):
-#  ym = pm(u)
-#  print('u='+repr(u)+' ym='+repr(ym))
+print("retry=" + str(concore.retrycount))
