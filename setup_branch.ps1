e653cd8b4f48146547ff110685c770294dc696f7
Avinash Kumar Deepak <avinash8655279@gmail.com>
2026-02-13 21:41:33 +0530

    use context managers for file I/O

--- setup_branch.ps1
+++
@@ -1,8 +0,0 @@
-cd "D:\gsoc org\concore-project\concore"
-git config core.editor "notepad"
-git fetch upstream
-git checkout dev
-git reset --hard upstream/dev
-git branch -D refactor-file-io-v2 2>$null
-git checkout -b refactor-file-io-v2
-Write-Host "Branch ready!"
