8cc3d50d3403d0aafe8e5fbe7035719a46ad8cdd
Sahil Lenka <sahillenka44@gmail.com>
2026-02-15 04:58:30 +0530

    Remove temporary helper scripts

--- fix_and_push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd C:\Users\Sahil\concore
-rmdir /s /q .git\rebase-merge 2>nul
-del /f .git\REBASE_HEAD 2>nul
-git reset --hard ad0f393
-git fetch upstream
-git merge upstream/dev -m "Merge upstream/dev"
-git push origin feature/enhanced-workflow-validation --force-with-lease
